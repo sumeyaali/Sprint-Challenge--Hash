816de664e85ead9008add3386443ef2bc7bc2bf8
sumeyaali <thesumeyaali@gmail.com>
2020-05-08 10:54:56 -0700

    not done

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,7 +1,22 @@
 def get_indices_of_item_weights(weights, length, limit):
+   
 
     """
     YOUR CODE HERE
+    
+    weight limit = limit
+    weights = [item weights]
     """
+    out_put = {}
+     
 
+    for i in range(length):
+        out_put[weights[i]] = i
+    print(out_put)
     return None
+
+weights = [ 4, 6, 10, 15, 16 ]
+get_indices_of_item_weights(weights, 5, 21)
+
+
+    
